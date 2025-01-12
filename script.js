a5de39b502fbdf958371d5679bf906f6ee07d181
moloshko <senya.klikun@mail.ru>
2025-01-12 16:57:06 +0300

    add click

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let header = document.querySelector("header")
 let start_btn = document.querySelector("#start_btn")
 let main = document.querySelector("main")
+let click_img = document.querySelector(".click-img")
+let weapon = document.querySelector(".weapon")
+let weapon_count = 0
 
 // обработка клика на кнопке -- Start --
 start_btn.addEventListener("click", () => {
@@ -8,4 +11,15 @@ start_btn.addEventListener("click", () => {
     main.style.display="flex"
     document.querySelector("body").style.backgroundImage="url(./Img/Wallpaper.png)"
     document.querySelector("body").style.alignItems="flex-start"
+})
+click_img.addEventListener("click", () => {
+    weapon_count++
+    weapon.innerHTML = weapon_count
+    console.log(weapon_count)
+    if (weapon_count % 2 === 1 ) {
+        click_img.innerHTML = "<img src='./Img/ClickActive.png'>"
+    }
+    else {
+        click_img.innerHTML = "<img src='./Img/ClickHover.png'>"
+    }
 })
