6fff04774ae2ea6cc48991995abcb9999a6625e5
moloshko <senya.klikun@mail.ru>
2025-01-18 16:41:22 +0300

    second

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let main = document.querySelector("main")
 let click_img = document.querySelector(".click-img")
 let weapon = document.querySelector(".weapon")
 let weapon_count = 0
+let count1 = 1
+let btn1 = document.querySelector(".btn1")
 
 // обработка клика на кнопке -- Start --
 start_btn.addEventListener("click", () => {
@@ -13,13 +15,19 @@ start_btn.addEventListener("click", () => {
     document.querySelector("body").style.alignItems="flex-start"
 })
 click_img.addEventListener("click", () => {
-    weapon_count++
+    weapon_count+=count1
     weapon.innerHTML = weapon_count
-    console.log(weapon_count)
+    //console.log(weapon_count)
     if (weapon_count % 2 === 1 ) {
         click_img.innerHTML = "<img src='./Img/ClickActive.png'>"
     }
     else {
         click_img.innerHTML = "<img src='./Img/ClickHover.png'>"
+    }
+})
+btn1.addEventListener("click", () => {
+    if (weapon_count >= 100) {
+        count1=2
+        weapon_count-=100
     }
 })
